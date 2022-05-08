8d76dfe98ac7d8d2f9873a4876778b9ee112aa93
Mateo Elosua <melosua@lcs.on.ca>
2022-05-08 17:20:13 -0400

    Initial Commit

---
+++ CulminatingProject/CulminatingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingProjectApp.swift
+//  CulminatingProject
+//
+//  Created by Mateo Elosua on 2022-05-08.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
